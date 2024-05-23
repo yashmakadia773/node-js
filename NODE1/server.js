12709ab56560c6e2763a10dc088c5d87a420d418
yashmakadia773 <133627880+yashmakadia773@users.noreply.github.com>
2024-05-23 09:16:31 +0530

    Add files via upload

---
+++ NODE1/server.js
@@ -0,0 +1,106 @@
+let http = require("http")
+
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3001, () => {
+//     console.log("xcscjbd");
+// })
+// // 2
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("dbhjajsbd")
+// }).listen(3002, () => {
+//     console.log("knmckkasjbd");
+// })
+// // 3
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("jajsbd")
+// }).listen(3003, () => {
+//     console.log("c;ldckasjbd");
+// })
+// // 4
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3004, () => {
+//     console.log("dkasjbd");
+// })
+// // 5
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3005, () => {
+//     console.log("dkascczxcjbd");
+// })
+// // 6
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3006, () => {
+//     console.log("dkasjbd");
+// })
+// // 7
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("lunsdbhjajsbd")
+// }).listen(3007, () => {
+//     console.log("dkasjbd");
+// })
+// // 8
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("jssdbhsbd")
+// }).listen(3008, () => {
+//     console.log("dkasjbd");
+// })
+// // 9
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3009, () => {
+//     console.log("dkasjbd");
+// })
+// // 10
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("jajsbd")
+// }).listen(3010, () => {
+//     console.log("dkasjbd");
+// })
+// // 11
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3011, () => {
+//     console.log("kasjbd");
+// })
+// // 12
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3012, () => {
+//     console.log("dkasjbd");
+// })
+// // 13
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3013, () => {
+//     console.log("dkasjbd");
+// })
+// // 14
+// http.createServer((req, res) => {
+//     console.log(req.header);
+//     res.end("ksdbhjajsbd")
+// }).listen(3014, () => {
+//     console.log("dkasjbd");
+// })
+// 15
+http.createServer((req, res) => {
+    console.log(req.header);
+    res.end("ksdbhjajsbd")
+}).listen(3015, () => {
+    console.log("dkasjbd");
+})
